311866337cfeb1bd111f4e187a4a39cf8124193e
Mark <mark@markjensen.dk>
2021-05-11 08:10:42 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Render
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var width = 100;
+            var height = 100;
+            var image = new bool[width, height];
+
+            var a = new Point2D(50, 10);
+            var b = new Point2D(80, 75);
+            var c = new Point2D(15, 92);
+
+            DrawTriangle(image, a, b, c);
+            ExportImage(image);
+        }
+
+        private static void DrawTriangle(bool[,] image, Point2D a, Point2D b, Point2D c)
+        {
+            // TODO only check bounding box of triangle
+            // TODO squares with early in/out
+
+            var width = image.GetLength(0);
+            var height = image.GetLength(1);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (Tools2D.PointInTriangle(new Point2D(x, y), a, b, c))
+                        image[x, y] = true;
+                }
+            }
+        }
+
+        private static void ExportImage(bool[,] image)
+        {
+            var width = image.GetLength(0);
+            var height = image.GetLength(1);
+            var builder = new StringBuilder();
+
+            builder.AppendLine("P3"); // ASCII PPM file header
+            builder.AppendLine(width + " " + height); // Dimensions of image
+            builder.AppendLine("255"); // Maximum colour value
+
+            for (int y = 0; y < height; y++)
+            {
+                var line = "";
+
+                for (int x = 0; x < width; x++)
+                {
+                    if (image[x, y])
+                        line += "0 0 0\t";
+                    else
+                        line += "255 255 255\t";
+                }
+
+                builder.AppendLine(line);
+            }
+
+            File.WriteAllText("output.ppm", builder.ToString());
+        }
+    }
+}
---
+++ Tools2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Render
+{
+    public static class Tools2D
+    {
+
+        public static bool PointInTriangle(Point2D p, Point2D a, Point2D b, Point2D c)
+        {
+            // Test if p lies inside triangle abc (counter clockwise)
+
+            // If p is to the right of ab then outside triangle
+            if (Point2D.Cross(p - a, b - a) < 0)
+                return false;
+
+            // If p is to the right of bc then outside triangle
+            if (Point2D.Cross(p - b, c - b) < 0)
+                return false;
+
+            // If p is to the right of ca then outside triangle
+            if (Point2D.Cross(p - c, a - c) < 0)
+                return false;
+
+            return true;
+        }
+
+        public static void UnitTest_PointInTriangle()
+        {
+            var a = new Point2D(0, 3);
+            var b = new Point2D(-3, -3);
+            var c = new Point2D(3, -3);
+
+            var test1 = PointInTriangle(new Point2D(0, 0), a, b, c);
+            var test2 = PointInTriangle(new Point2D(1, 4), a, b, c);
+            var test3 = PointInTriangle(new Point2D(4, 1), a, b, c);
+            var test4 = PointInTriangle(new Point2D(1, 1), a, b, c);
+
+            Console.WriteLine("test1: " + test1);
+            Console.WriteLine("test2: " + test2);
+            Console.WriteLine("test3: " + test3);
+            Console.WriteLine("test4: " + test4);
+        }
+    }
+}
